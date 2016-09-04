5edc11bdb2285788ba4f1752c2342b5b91041f3a
Kevin Morris <kmorris511@yahoo.com>
2016-09-03 21:33:22 -0600

    Starting swipe delete of factors

--- app/src/main/kotlin/com/mountainowl/headachewizard/ui/EditFactorsFragment.kt
+++ app/src/main/kotlin/com/mountainowl/headachewizard/ui/EditFactorsFragment.kt
@@ -4,14 +4,9 @@ import android.app.AlertDialog
 import android.app.ListFragment
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.view.inputmethod.InputMethodManager
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 import com.mountainowl.headachewizard.R
 import com.mountainowl.headachewizard.model.DataManager
 import com.mountainowl.headachewizard.model.Factor
@@ -55,12 +50,6 @@ class EditFactorsFragment : ListFragment() {
         return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        listView.onItemClickListener = FactorClickListener()
-    }
-
     private fun displayDuplicateFactorDialog(duplicateFactorName: String) {
 
         val dialogBuilder = AlertDialog.Builder(activity)
@@ -81,6 +70,11 @@ class EditFactorsFragment : ListFragment() {
         dialog.show()
     }
 
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+
+
+    }
+
     private inner class EditFactorsAdapter(factors: List<Factor>) : ArrayAdapter<Factor>(activity, android.R.layout.simple_list_item_1, factors) {
 
         override fun getView(position: Int, view: View?, parent: ViewGroup): View {
@@ -89,13 +83,41 @@ class EditFactorsFragment : ListFragment() {
             val factorNameField = newView.findViewById(R.id.factor_name) as TextView
             factorNameField.text = getItem(position).name
 
-            val deleteFactorButton = newView.findViewById(R.id.delete_factor_button) as Button
-            deleteFactorButton.setOnClickListener {
-                val factorName = getItem(position).name
-                displayDeleteFactorConfirmationDialog(factorName)
-            }
+            val nameContainer = newView.findViewById(R.id.list_item_edit_factor_name_container)
+            newView.setOnTouchListener(FactorTouchListener(position, newView, context))
 
             return newView
+        }
+    }
+
+    private inner class FactorTouchListener(private val position: Int, private val view: View, context: Context) : View.OnTouchListener {
+
+        val gestureDetector = GestureDetector(context, SwipeListener())
+
+        override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+            return gestureDetector.onTouchEvent(event)
+        }
+
+        private inner class SwipeListener : GestureDetector.SimpleOnGestureListener() {
+
+            override fun onDown(e: MotionEvent) = true
+
+            override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+
+                val deleteFactorButton = view.findViewById(R.id.list_item_edit_factor_delete) as TextView
+                val nameContainer = view.findViewById(R.id.list_item_edit_factor_name_container)
+
+                nameContainer.translationX = if(nameContainer.translationX < 0) 0.0f else -300.0f
+
+                val deleteFactorButtonClicked = View.OnClickListener {
+                    val factorName = adapter.getItem(position).name
+                    displayDeleteFactorConfirmationDialog(factorName)
+                }
+
+                deleteFactorButton.setOnClickListener(if(nameContainer.translationX < 0) deleteFactorButtonClicked else null)
+
+                return false
+            }
         }
     }
 
